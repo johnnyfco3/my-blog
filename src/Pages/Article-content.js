6b6ccb224941446b75b48b2d3e5f15bf23476ed4
Johnny Tejada <johnnyfran20002@gmail.com>
2022-01-18 15:56:00 -0500

    finished course

--- src/Pages/Article-content.js
+++ src/Pages/Article-content.js
@@ -1,11 +1,11 @@
 const articles = [
     {
-        name: 'learn-react',
+        name: "learn-react",
         title: 'SOME BIG TITLE',
         content: ['Here is some content as example']
     },
     {
-        name: 'example',
+        name: "learn-node",
         title: 'Example Article',
         content: ['Words words worjfnvjf vfvnfk vfjnv fnv fv jfvnfjkv vfnvkf v',
         'kfjkfjnv vkjnfv vnvknv ']
--- src/Pages/ArticlePage.js
+++ src/Pages/ArticlePage.js
@@ -1,24 +1,42 @@
-import React from 'react';
+ import {React, useState, useEffect } from 'react';
 import {useParams} from 'react-router-dom';
 import ArticleList from '../Components/ArticleList';
 import NotFoundPage from './NotFoundPage';
 import articles from './Article-content';
+import CommentsList from '../Components/CommentsList';
+import UpvotesSection from '../Components/UpvotesSection';
+import AddCommentForm from '../Components/AddCommentForm';
 
 
 function ArticlePage(){
     const {name} = useParams()
     const article = articles.find(article => article.name === name);
+    
+    const[articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: []});
+    
+    useEffect(()=> {
+        const fetchData = async () =>{
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
+
     if(!article) return <NotFoundPage />
     const otherArticles = articles.filter(article => article.name !== name)
 
     return(
         <>
         <h1>{article.title}</h1>
-       {article.content.map((paragraph, key) =>(
-           <p key={key}>{paragraph}</p>
-       ))}
-       <h3>Other Articles: </h3>
-       <ArticleList article={otherArticles} />
+        <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo} />
+        {article.content.map((paragraph, key) =>(
+            <p key={key}>{paragraph}</p>
+        ))}
+        <CommentsList comments={articleInfo.comments} />
+        <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
+        <h3>Other Articles: </h3>
+        <ArticleList article={otherArticles} />
         </>
     );
 };
